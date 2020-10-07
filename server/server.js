cfca326d60645265e6ce0ffc64654790a5bbb5b6
Andrea RH <andrea.r.hamilton92@gmail.com>
2020-10-07 12:57:06 -0400

    created config files. connected to mongoDB atlas

--- server/server.js
+++ server/server.js
@@ -2,25 +2,30 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const mongoose= require('mongoose');
-require('dotenv').config()
+const config = require('config');
+const connectDB = require('./config/db');
+const dotenv = require('dotenv');
+dotenv.config({ path: 'ENV_FILENAME' });
 
 const app = express();
 
+connectDB();
+
 // import routes
-const authRoutes = require('./routes/auth')
+const authRoutes = require('./routes/auth');
 
 // app middlewares 
 app.use(morgan('dev'));
 app.use(express.json({ extended: false }))
 // app.use(cors()); // allows all origins
-if (process.env.NODE_ENV = 'development') {
+if (config.nodeENV = 'development') {
     app.use(cors({ origin: `http://localhost:3000` }))
 }
 
 // middleware
 app.use('/api', authRoutes)
 
-const PORT = process.env.port || 8000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
-    console.log(`API is running on port: ${PORT}`)
+    console.log(`API is running on port: ${PORT} - ${config.nodeENV}`)
 })
