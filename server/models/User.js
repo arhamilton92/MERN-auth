778d697d78f5ceb5b921804f84fac3c444d810bd
Andrea RH <andrea.r.hamilton92@gmail.com>
2020-10-07 12:01:01 -0400

    created methods and virtual fields for User.js model to hash password.

--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const crypto = require('crypto');
+const { match } = require('assert');
 
 // user schema
 const userSchema = new Mongoose.Schema({
@@ -26,12 +27,43 @@ const userSchema = new Mongoose.Schema({
         default: 'subscriber'
     },
     resetPasswordLink: {
-        type: String,
+        data: String,
         default: ''
     }
 }, {timestamps: true})
 
+
 // virtual
+userSchema.virtual('password')
+.set(function(password) {
+    this._password = password
+    this.salt = this.makeSalt()
+    this.hashed_password = this.encryptPassword(password)
+})
+.get(function() {
+    return this._password
+})
 
 
 // methods
+userSchema.methods = {
+    authenticate: function(plainText) {
+        return this.encryptPassword(plainText) === this.hashed_password;
+    },
+    encryptPassword: function(password) {
+        if(!password) return ''
+        try {
+            return crypto
+                .createHmac('sha1', this.salt)
+                .update(password)
+                .digest('hex');
+        } catch (error) {
+            
+        }
+    },
+    makeSalt: function() {
+        return Math.round(new Date().valueOf() * Math.random()) + ''
+    }
+};
+
+module.exports = mongoose.model('User', userSchema);
--- server/server.js
+++ server/server.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+// const bodyParser = require('body-parser');
+const mongoose= require('mongoose');
+require('dotenv').config()
 
 const app = express();
 
 // import routes
 const authRoutes = require('./routes/auth')
+
+// app middlewares 
+app.use(morgan('dev'));
+app.use(express.json({ extended: false }))
+// app.use(cors()); // allows all origins
+if (process.env.NODE_ENV = 'development') {
+    app.use(cors({ origin: `http://localhost:3000` }))
+}
 
 // middleware
 app.use('/api', authRoutes)
